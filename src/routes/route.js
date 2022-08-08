6ff278234b0a686f2890d5ca888b7444ff09897a
Saquib <saquibparvez007@gmail.com>
2022-08-08 22:47:11 +0530

    Apis

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,93 @@ router.get('/students', function (req, res){
     res.send(students)
 })
 
+//============================================================================================================================
+//problem-1
+
+router.get('/No movie exists with this id’
+
+',function(req,res){
+    let Movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(Movies)
+})
+
+//Problem-2
+
+router.get('/movies/:indexNumber',function(req,res){
+    let movie=['Tarzan', 'Raid', 'Lord of the rings', 'Batman begins','Ms.Dhoni']
+    
+        let showmMovie
+        if(req.params.indexNumber<movie.length){
+            showmMovie=movie[req.params.indexNumber]
+        }else{
+            showmMovie="use a valid movie index"
+        }
+    
+    res.send(showmMovie)
+
+})
+
+
+//Problem 3
+
+
+router.get('/films',function(req,res){
+    const films=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+
+       res.send(films)
+       
+})
+
+//problem 5
+router.get('/films/:filmId',function(req,res){
+    const films=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+
+       let film
+
+       if(req.params.filmId<=films.length&&req.params.filmId!=0){
+        for(i=0;i<films.length;i++){
+            if(req.params.filmId==films[i].id){
+                film=films[i]
+                break
+            }
+            
+            else{
+                film="No movie exists with this id "
+            }
+        }
+       }
+res.send(film)
+       
+})
+
+
+
+
+//========================================================================================================================
 router.get('/student-details/:name', function(req, res){
     /*
     params is an attribute inside request that contains 
